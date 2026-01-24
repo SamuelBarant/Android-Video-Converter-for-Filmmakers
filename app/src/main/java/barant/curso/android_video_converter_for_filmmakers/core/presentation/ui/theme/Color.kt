ea73fa98bf35699396dc6fb8eaeee617e3cfe748
SamuelBarant <samucraft346@gmail.com>
2026-01-24 01:09:57 +0100

    App theme.

---
+++ app/src/main/java/barant/curso/android_video_converter_for_filmmakers/core/presentation/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package barant.curso.android_video_converter_for_filmmakers.core.presentation.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Background = Color(0xFF10292C)
+val OnBackground = Color(0xFF10292C)
+val Primary = Color(0xFFFFFFFF)
+val ContainerPrimary = Color(0xFF13D8EA)
+val OnContainerPrimary = Color(0xFF102224)
+val Secondary = Color(0xFF91A0B4)
+val ContainerSecondary = Color(0xFF117C86)
+val OnContainerSecondary = Color(0xFF102022)
+val Surface = Color(0xFF1C2B2D)
--- app/src/main/java/barant/curso/android_video_converter_for_filmmakers/core/presentation/ui/theme/Theme.kt
+++ app/src/main/java/barant/curso/android_video_converter_for_filmmakers/core/presentation/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
-package barant.curso.android_video_converter_for_filmmakers.ui.theme
+package barant.curso.android_video_converter_for_filmmakers.core.presentation.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -12,31 +11,32 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Primary,
+    primaryContainer = ContainerPrimary,
+    onPrimaryContainer = OnContainerPrimary,
+    secondary = Secondary,
+    secondaryContainer = ContainerSecondary,
+    onSecondaryContainer = OnContainerSecondary,
+    background = Background,
+    onBackground = OnBackground,
+    surface = Surface
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = Primary,
+    primaryContainer = ContainerPrimary,
+    onPrimaryContainer = OnContainerPrimary,
+    secondary = Secondary,
+    secondaryContainer = ContainerSecondary,
+    onSecondaryContainer = OnContainerSecondary,
+    background = Background,
+    onBackground = OnBackground,
+    surface = Surface
 )
 
 @Composable
 fun AndroidVideoConverterforFilmmakersTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
@@ -53,6 +53,7 @@ fun AndroidVideoConverterforFilmmakersTheme(
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
+        shapes = Shapes,
         content = content
     )
 }
