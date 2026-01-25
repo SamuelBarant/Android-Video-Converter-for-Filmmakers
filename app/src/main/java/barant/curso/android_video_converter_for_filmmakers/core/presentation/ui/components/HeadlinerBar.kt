d6f1a173ef31114765fbdc25c46e7c07e29e91eb
SamuelBarant <samucraft346@gmail.com>
2026-01-25 18:49:01 +0100

    PR corrections.

--- app/src/main/java/barant/curso/android_video_converter_for_filmmakers/core/presentation/ui/components/HeadlinerBar.kt
+++ app/src/main/java/barant/curso/android_video_converter_for_filmmakers/core/presentation/ui/components/HeadlinerBar.kt
@@ -1,6 +1,5 @@
 package barant.curso.android_video_converter_for_filmmakers.core.presentation.ui.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -10,16 +9,15 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HeadlinerBar(
     modifier: Modifier = Modifier,
-    navigationIcon: @Composable () -> Unit,
+    navigationIcon: @Composable () -> Unit = {},
     label: String,
-    actions: @Composable RowScope.() -> Unit
+    actions: @Composable RowScope.() -> Unit = {}
 ){
     TopAppBar(
         modifier = modifier,
